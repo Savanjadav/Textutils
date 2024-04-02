dab12a1cf8957d0755e075deaf997aa61fbd11bd
SAVAN JADAV <savan.p.jadav@gmail.com>
2024-04-02 12:41:59 +0530

    Video 19 completed

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -2,14 +2,12 @@ import React, {useState} from 'react';
 
 export default function TextForm(props) {
 	const handleUpClick=()=>{
-		console.log("Uppercase was clicked: "+text);
 		let newText=text.toUpperCase();
 		setText(newText);
 		props.showAlert("Converted to uppercase", "success");
 	}
 	
 	const handleLoClick=()=>{
-		console.log("Lowercase was clicked: "+text);
 		let newText=text.toLowerCase();
 		setText(newText);
 		props.showAlert("Converted to lowercase", "success");
@@ -22,7 +20,6 @@ export default function TextForm(props) {
 	}
 	
 	const handleOnChange=(event)=>{
-		// console.log("On change");
 		setText(event.target.value);
 	}
 	
@@ -49,7 +46,7 @@ export default function TextForm(props) {
 			</div>
 			<div className="container my-3" style={{color: props.mode==='dark'?'white':'black'}}>
 				<h1>Your text summary</h1>
-				<p>{text.split(" ").filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
+				<p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
 				<p>{0.008 * text.split(" ").filter((element)=>{return element.length!==0}).length} Minutes read</p>
 				<h2>Preview</h2>
 				<p>{text.length>0?text:"Nothing to preview"}</p>
